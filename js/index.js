df2a6613b50ba3f4cb3ebf2a7476d4b06023837c
Lelly Oliver <95884484+lellyoliver@users.noreply.github.com>
2025-07-25 20:15:41 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 //gets the variables
 let strName, strPhone, strEmail, strMedia, strDate
-const btn = $('#btnSubmit')
+const btn = document.getElementById('btnSubmit')
 
-//get the value HMTL
-$('#btnSubmit').on('click', (e) =>{
+//get the value HTML
+document.getElementById('btnSubmit').addEventListener('click', (e) => {
     e.preventDefault()
-    strName = $('#name').val()
-    strPhone = $('#phone').val()
-    strEmail = $('#email').val()
-    strMedia = $('#media').val()
-    strDate = $('#date').val()
+    strName = document.getElementById('name').value
+    strPhone = document.getElementById('phone').value 
+    strEmail = document.getElementById('email').value
+    strMedia = document.getElementById('media').value
+    strDate = document.getElementById('date').value
 })
 
 function getStrUrl(name, phone){
     //clear string
-    name = name.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    name = name.replace(/[( )]/g, "%20");
-    phone = phone.replace(/[\(\)\.\s-]+/g,'');
+    name = name.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+    name = name.replace(/[( )]/g, "%20")
+    phone = phone.replace(/[\(\)\.\s-]+/g,'')
 
     //join string
     const url = 'https://api.whatsapp.com/send?phone=1131674145&text='
-    const concatURL = url + 'Nome:%20' + name + '%0ATelefone:%20' + phone + '%0AMail:%20' + email + '%0AQnt%20de%20pessoas:%20' + media + '%0AData%20do%20evento:%20'
-    console.log(concatURL);
+    const concatURL = url + encodeURIComponent('Nome: ' + name + '\nTelefone: ' + phone + '\nMail: ' + strEmail + '\nQnt de pessoas: ' + strMedia + '\nData do evento: ' + strDate)
+    console.log(concatURL)
 }
 
 //get the string
